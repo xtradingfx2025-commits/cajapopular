3c9541915525c9b1ecb602da9cebc6ddcd2717b0
xtradingfx2025-commits <xtradingfx2025@gmail.com>
2025-11-20 11:35:41 -0600

    Update form.tsx

--- frontend/src/components/ui/form.tsx
+++ frontend/src/components/ui/form.tsx
@@ -13,7 +13,7 @@ import {
   type FieldValues,
 } from "react-hook-form"
 
-import { cn } from "/src/lib/lib/utils"
+import { cn } from "/src/lib/utils"
 import { Label } from "/src/lib/components/ui/label"
 
 const Form = FormProvider
